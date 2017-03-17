ebccc828604110290daca61f8943320ff5a17d73
JoseMoranMu <josemoranmu@gmail.com>
2017-03-17 10:21:37 +0100

    17/03/2017v2
    movimientos de barril

--- core/src/com/bstq/Game/Barril.java
+++ core/src/com/bstq/Game/Barril.java
@@ -2,7 +2,11 @@ package com.bstq.Game;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+
+import static com.badlogic.gdx.scenes.scene2d.actions.Actions.moveBy;
 
 /**
  * Created by Jose on 10/03/2017.
@@ -35,5 +39,9 @@ public class Barril extends Actor {
     }
     public void setLeft(boolean left){
         this.left=left;
+    }
+    public void move(){
+
+        //this.addAction(moveBy(51, 0));
     }
 }
