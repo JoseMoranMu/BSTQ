8966398ce851705e837380147db9f97861848055
Nitryus <nitryus.typhoon.oxide@gmail.com>
2017-03-31 10:32:04 +0200

    Clase Login y LoginMenu

---
+++ core/src/com/bstq/view/Login.java
@@ -0,0 +1,57 @@
+package com.bstq.view;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by alumne on 31/03/2017.
+ */
+
+public class Login implements Input.TextInputListener, Screen {
+
+
+    @Override
+    public void input(String text) {
+
+    }
+
+    @Override
+    public void canceled() {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/com/bstq/view/LoginMenu.java
@@ -0,0 +1,83 @@
+package com.bstq.view;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.bstq.Main;
+
+/**
+ * Created by alumne on 31/03/2017.
+ */
+
+//   https://github.com/libgdx/libgdx/wiki/Simple-text-input
+
+public class LoginMenu extends Login {
+    final Main main;
+    Button lvl1;
+    Stage stage;
+
+    public LoginMenu(Main main) {
+        Gdx.input.setCatchBackKey(true);
+        this.main=main;
+        stage = new Stage();
+        prepareButtons();
+        prepareListeners();
+        stage.addActor(lvl1);
+    }
+
+    private void prepareListeners() {
+        lvl1.addCaptureListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                ButtonHandler bh = new ButtonHandler();
+                lvl1=bh.getButton(new Texture(Gdx.files.internal("level-1.png")),300,1300);
+            }
+        });
+    }
+
+    private void prepareButtons() {
+    }
+
+    @Override
+    public void hide() {
+        Gdx.input.setInputProcessor(null);
+        stage.dispose();
+
+    }
+
+    @Override
+    public void render(float delta){
+        Gdx.gl.glClearColor(0, 0.5f, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act();
+        stage.draw();
+        BackButton();
+    }
+
+    private void BackButton() {
+        if (Gdx.input.isKeyPressed(Input.Keys.BACK)) {
+            main.setScreen(new MainMenu(main));
+        }
+    }
+
+    @Override
+    public void show(){
+        Gdx.input.setInputProcessor(stage);
+        Gdx.input.setCatchBackKey(true);
+    }
+    @Override
+    public void dispose() {
+        stage.dispose();
+
+    }
+
+    /* https://github.com/libgdx/libgdx/wiki/File-handling
+    FileHandle file = Gdx.files.local("myfile.txt");
+file.writeString("My god, it's full of stars", false);*/
+
+}
