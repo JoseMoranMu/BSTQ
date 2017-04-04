5e45d8d452c6bc4ff3f13d57f4609fc8ab4f8739
Nitryus <nitryus.typhoon.oxide@gmail.com>
2017-04-04 11:30:13 +0200

    Login Menu tests

--- core/src/com/bstq/view/LoginMenu.java
+++ core/src/com/bstq/view/LoginMenu.java
@@ -7,19 +7,32 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.bstq.Main;
+
+import static com.badlogic.gdx.graphics.profiling.GLProfiler.listener;
 
 /**
  * Created by alumne on 31/03/2017.
  */
 
 //   https://github.com/libgdx/libgdx/wiki/Simple-text-input
+//   https://github.com/libgdx/libgdx/wiki/On-screen-keyboard
+
+/** OVERLAP 2D
+ * https://www.youtube.com/watch?v=Lp1GRyUUoFM overlap2D
+ * https://www.youtube.com/watch?v=yeD8lVYwAWQ
+  */
+
+
 
 public class LoginMenu extends Login {
     final Main main;
     Button lvl1;
     Stage stage;
+    //MyTextInputListener textInputListener;
+    String txtUsername;
 
     public LoginMenu(Main main) {
         Gdx.input.setCatchBackKey(true);
@@ -28,14 +41,23 @@ public class LoginMenu extends Login {
         prepareButtons();
         prepareListeners();
         stage.addActor(lvl1);
+
+        /*txtUsername = new TextField("", stage);
+        txtUsername.setMessageText("test");
+        txtUsername.setPosition(30, 30);
+        stage.addActor(txtUsername);
+        String test = txtUsername.getText();
+        System.out.println(test);*/
     }
 
     private void prepareListeners() {
         lvl1.addCaptureListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                ButtonHandler bh = new ButtonHandler();
-                lvl1=bh.getButton(new Texture(Gdx.files.internal("level-1.png")),300,1300);
+                /*pgs = new PlayGameScreen(main);
+                main.setScreen(pgs);*/
+                /*MyTextInputListener listener = new MyTextInputListener();
+                Gdx.input.getTextInput(listener, "Dialog Title", "Initial Textfield Value", "Hint Value");*/
             }
         });
     }
@@ -58,13 +80,6 @@ public class LoginMenu extends Login {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act();
         stage.draw();
-        BackButton();
-    }
-
-    private void BackButton() {
-        if (Gdx.input.isKeyPressed(Input.Keys.BACK)) {
-            main.setScreen(new MainMenu(main));
-        }
     }
 
     @Override
@@ -80,11 +95,14 @@ public class LoginMenu extends Login {
 
     @Override
     public void input (String text) {
+        Gdx.input.setOnscreenKeyboardVisible(true);
     }
 
     @Override
     public void canceled () {
-        System.out.println("holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        if (Gdx.input.isKeyPressed(Input.Keys.BACK)) {
+            main.setScreen(new MainMenu(main));
+        }
     }
 
     /* https://github.com/libgdx/libgdx/wiki/File-handling
