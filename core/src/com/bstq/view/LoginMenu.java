24bbe9dc45699f9a05e90ceef73e0b92897b349b
Nitryus <nitryus.typhoon.oxide@gmail.com>
2017-05-09 10:35:15 +0200

    Textfield tests

--- core/src/com/bstq/view/LoginMenu.java
+++ core/src/com/bstq/view/LoginMenu.java
@@ -1,12 +1,16 @@
 package com.bstq.view;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.bstq.Main;
@@ -34,6 +38,11 @@ public class LoginMenu extends Menu  {
     //MyTextInputListener textInputListener;
     String txtUsername;
     Login listener = new Login();
+    SpriteBatch b;
+    BitmapFont font;
+    String email, password;
+    TextField emailTF,passwordTF;
+    Skin skin;
 
     /*TextField textfield= new TextField("", skin);
     textfield.setSize(300, 50);
@@ -48,6 +57,12 @@ public class LoginMenu extends Menu  {
         prepareButtons();
         prepareListeners();
         stage.addActor(lvl1);
+        font = new BitmapFont();
+        font.getData().setScale(5,5);
+        b= new SpriteBatch();
+        skin = new Skin();
+        skin.add("textfield","textfield.png");
+
 
         /*txtUsername = new TextField("", stage);
         txtUsername.setMessageText("test");
@@ -61,6 +76,10 @@ public class LoginMenu extends Menu  {
         lvl1.addCaptureListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
+                Gdx.input.setInputProcessor(stage);
+                Gdx.input.setCatchBackKey(true);
+                Gdx.input.getTextInput(listener, "Dialog Title", "Initial Textfield Value", "Hint Value");
+                ///////////////////////////////////////email=listener;
                 /*pgs = new PlayGameScreen(main);
                 main.setScreen(pgs);*/
                 /*MyTextInputListener listener = new MyTextInputListener();
@@ -87,13 +106,15 @@ public class LoginMenu extends Menu  {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act();
         stage.draw();
+        b.begin();
+        font.draw(b,"Email: ",400,1440);
+        //font.draw(b,"Password: ",400,1340);
+        b.end();
     }
 
     @Override
     public void show(){
-        Gdx.input.setInputProcessor(stage);
-        Gdx.input.setCatchBackKey(true);
-        Gdx.input.getTextInput(listener, "Dialog Title", "Initial Textfield Value", "Hint Value");
+
     }
     @Override
     public void dispose() {
