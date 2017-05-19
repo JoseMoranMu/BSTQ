ba6698ebd6bc8a20fdede789c82354f84808b617
JoseMoranMu <josemoranmu@gmail.com>
2017-05-19 15:54:22 +0200

    Clase Ranking

---
+++ core/src/com/bstq/Service/Ranking.java
@@ -0,0 +1,23 @@
+package com.bstq.Service;
+
+import java.util.List;
+
+/**
+ * Created by Jose on 19/05/2017.
+ */
+
+public class Ranking {
+    List<User> list;
+
+    public Ranking(){
+
+    }
+
+    public List<User> getList() {
+        return list;
+    }
+
+    public void setList(List<User> list) {
+        this.list = list;
+    }
+}
